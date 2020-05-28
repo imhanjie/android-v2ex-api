6e82f688c11a884a5b2579b1d0b18a51f6dcf217
hanjie <95hanjie@gmail.com>
2020-05-28 09:55:29 +0800

    feat: 动态 header 处理

--- library/src/main/java/com/imhanjie/v2ex/api/HeaderInterceptor.kt
+++ library/src/main/java/com/imhanjie/v2ex/api/HeaderInterceptor.kt
@@ -1,5 +1,6 @@
 package com.imhanjie.v2ex.api
 
+import android.annotation.SuppressLint
 import com.imhanjie.v2ex.api.support.V2exConstants
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -9,18 +10,33 @@ import okhttp3.Response
  */
 class HeaderInterceptor : Interceptor {
 
+    @SuppressLint("DefaultLocale")
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        val requestUrl = request.url().toString()
+        val requestUrl = request.url().toString().replace(V2exConstants.BASE_URL, "")
 
         // 收藏 & 取消收藏主题
-        if (requestUrl.startsWith("${V2exConstants.BASE_URL}/favorite/topic/") || requestUrl.startsWith("${V2exConstants.BASE_URL}/unfavorite/topic/")) {
-            val topicId = requestUrl.split("?")[0].split("/").last()
-            request = request.newBuilder().apply {
-                header("Referer", "${V2exConstants.BASE_URL}/t/$topicId")
-            }.build()
-        }
-
+        request =
+            if (requestUrl.startsWith("/favorite/topic/") || requestUrl.startsWith("/unfavorite/topic/")) {
+                val topicId = requestUrl.split("?")[0].split("/").last()
+                request.newBuilder().apply {
+                    header("Referer", "${V2exConstants.BASE_URL}/t/$topicId")
+                }.build()
+            } else if (requestUrl.startsWith("/_captcha")) {
+                request.newBuilder().apply {
+                    header("Referer", "${V2exConstants.BASE_URL}/signin")
+                }.build()
+            } else if (requestUrl.startsWith("/signin") && request.method().toUpperCase() == "POST") {
+                request.newBuilder().apply {
+                    header("Referer", "${V2exConstants.BASE_URL}/signin")
+                }.build()
+            } else if (requestUrl.startsWith("/favorite/node/") || requestUrl.startsWith("/unfavorite/node/")) {
+                request.newBuilder().apply {
+                    header("Referer", "${V2exConstants.BASE_URL}/go")
+                }.build()
+            } else {
+                request
+            }
         return chain.proceed(request)
     }
 
